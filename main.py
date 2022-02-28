ad983ccbaff3b4213b6a2f84b1c799feb7090173
Aniket Kshirsagar <74361483+4hund3rbird@users.noreply.github.com>
2022-02-28 15:44:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
-
-
+#global constants--------------------------------------------------------
+FONT=('arial',20,"italic")
+WIN_HEIGHT=500
+WIN_WIDTH=400
 
 
 #UI setup-----------------------------------------------------------------
@@ -14,15 +16,18 @@ def create_circle(x, y, r, canvasName): #center coordinates, radius
 
 
 window=Tk()
-window.config(width=400,height=500,bg="black",highlightthickness=0,padx=50,pady=50)
+window.config(width=WIN_WIDTH,height=WIN_HEIGHT,bg="black",highlightthickness=0)
 window.title("Pomodoro application")
+window.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
 
-time=Label(text="00:01",font=('arial',20,"italic"),fg="white",bg="black")
-# create_circle(200,250,10,circle_canvas)
+
+
 
 
 #timer mechanism---------------------------------------------------------
-
+def timer():
+    time=Label(text="00:01",font=FONT,fg="white",bg="black",highlightthickness=0)
+    time.pack()
 #break and work count mechanism------------------------------------------
 #reset mechanism---------------------------------------------------------
 
